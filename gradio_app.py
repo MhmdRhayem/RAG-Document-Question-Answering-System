8147d9dd5b4ac1dba8198e607ad2692f869ceaec
MhmdRhayem <10121011@mu.edu.lb>
2024-11-29 18:04:02 +0200

    Add upload_document function to handle file uploads and processing

--- gradio_app.py
+++ gradio_app.py
@@ -6,3 +6,20 @@ import os
 API_URL = "http://127.0.0.1:5000"
 UPLODAD_FOLDER = "./docs"
 history = ""
+
+def upload_document(file):
+    try:
+        if not os.path.exists(UPLODAD_FOLDER):
+            os.makedirs(UPLODAD_FOLDER)
+        shutil.copy(file, UPLODAD_FOLDER)
+        files = {'file': file}
+        gr.Info("Wait while uploading the document and processing ...")
+        response = requests.post(f"{API_URL}/upload", files=files)
+        if response.status_code != 200:
+            return f"Error: {response.text}"
+        gr.Info(f"Done Uploading and Preprocessing ...")
+        return gr.update(interactive=True)
+    except requests.exceptions.RequestException as e:
+        return f"Request failed: {str(e)}"
+    except ValueError as e:
+        return f"Failed to parse response: {str(e)}"
